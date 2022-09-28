2451a55c410f55ad286482b80be80083e9388ff8
arungetz4u <113575647+arungetz4u@users.noreply.github.com>
2022-09-28 12:38:14 +0530

    Add files via upload

---
+++ pythoncollections-list.py
@@ -0,0 +1,20 @@
+# list
+list1 = ["india", "australia", "england"]
+print (list1)
+print (len(list1)) # no of items in list
+print(list1[0]) # to print the item with index value
+list1[0] = "kerala" # to change the values of a list using index value
+print(list1)
+list2 = [10, 20, 30, 40]
+sum = list2[1] + list2[3] # sum of list2 numbers with their index value
+print(sum)
+list1.append("africa") # to append(insert) a new value in to the list
+print(list1)
+list1.remove("africa") # to remove a value from list
+print(list1)
+list1.insert(0, "africa") # to insert a value to the desired index
+print(list1)
+list1.clear() # to clear a list
+print(list1)
+# del list1 # to delete a list
+# print(list1)
