97e646c1b7fd500e79a532e24b4e6b0630514979
arungetz4u <113575647+arungetz4u@users.noreply.github.com>
2022-09-28 11:41:24 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+text = "  Arun Rajan"
+print(len(text))
+print(text.strip())
+print(text)
+print(text.lower())
+print(text.upper())
+print(text.replace("R","P"))
+a = "Learn "
+b = "Python "
+d = "with Google"
+c = a + b + d
+print(c)
